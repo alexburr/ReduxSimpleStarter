a586d679dafed99092371c1e2a65f721c1d88b52
Alex Burr <aburr@pharos.com>
2018-05-25 11:09:42 -0400

    Update to create a component class 'App'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create new component. This component should produce 
 // some HTML
 const App = function() {
@@ -6,4 +9,4 @@ const App = function() {
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-React.render(App);
+ReactDOM.render(App);
